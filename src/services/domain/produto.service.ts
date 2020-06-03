d291ff0126edc740fee5a697c7cf6411ee8992f5
Rafael Marinheiro <rsmarinheiro007@gmail.com>
2020-06-03 01:12:04 -0300

    Carregando imagens de Produtos via S3

--- src/services/domain/produto.service.ts
+++ src/services/domain/produto.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "../../../node_modules/@angular/core";
 import { HttpClient } from "../../../node_modules/@angular/common/http";
 import { API_CONFIG } from "../../config/api.config";
+import { Observable } from "rxjs/Rx";
 
 @Injectable()
 export class ProdutoService{
@@ -12,6 +13,12 @@ export class ProdutoService{
     findByCategoria(categoria_id : string){
     console.log(categoria_id);
     return this.http.get(`${API_CONFIG.baseUrl}/produto/?categorias=${categoria_id}`) 
+    }
+
+    getSmallImageFromBucket(id :string):Observable<any>{
+        let url = `${API_CONFIG.bucketBaseUrl}/prod${id}-small.jpg`
+        return this.http.get(url , {responseType : 'blob'});
+
     }
 
 }
