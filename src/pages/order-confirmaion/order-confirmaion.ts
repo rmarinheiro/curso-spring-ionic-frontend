93f733924a40a645dfaa5fd997346d0ab1ebcc3f
Rafael Marinheiro <rsmarinheiro007@gmail.com>
2020-06-17 00:58:21 -0300

    Implementação do pedido registrado

--- src/pages/order-confirmaion/order-confirmaion.ts
+++ src/pages/order-confirmaion/order-confirmaion.ts
@@ -19,7 +19,8 @@ export class OrderConfirmaionPage {
   pedido : PedidoDTO;
   cartItems : CartItem[];
   cliente : ClienteDTO;
-  endereco : EnderecoDTO
+  endereco : EnderecoDTO;
+  codpedido : string;
 
   constructor(public navCtrl: NavController, public navParams: NavParams,
               public cartService:CartService,
@@ -55,17 +56,27 @@ export class OrderConfirmaionPage {
     this.navCtrl.setRoot('CartPage');
   }
 
+  home(){
+    this.navCtrl.setRoot('CategoriasPage');
+  }
+
   checkout(){
     this.pedidoService.insert(this.pedido)
     .subscribe(response => {
         this.cartService.createOrClearCart();
-        console.log(response.headers.get('location'));
+       // console.log(response.headers.get('location'));
+       this.codpedido = this.extractId(response.headers.get('location'));
     },
   error=>{
     if(error.status == 403){
       this.navCtrl.setRoot('HomePage')
     }
   });
+  }
+
+  private extractId(location : string):string{
+    let position = location.lastIndexOf('/');
+    return location.substring(position + 1,location.length);
   }
 
 }
