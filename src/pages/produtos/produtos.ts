52d106b46d29e7c5041c3b49006ae058c5521f51
Rafael Marinheiro <rsmarinheiro007@gmail.com>
2020-06-17 01:32:17 -0300

    Implementação do Loading e do Refresh

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-angular';
 import { ProdutoDTO } from '../../models/produto.dto';
 import { ProdutoService } from '../../services/domain/produto.service';
 import { API_CONFIG } from '../../config/api.config';
@@ -22,19 +22,26 @@ export class ProdutosPage {
 
   constructor(public navCtrl: NavController, 
               public navParams: NavParams,
-            public produtoService : ProdutoService) {
+            public produtoService : ProdutoService,
+          public loadingController:LoadingController) {
   }
 
   ionViewDidLoad() {
+      this.loadData();
+  }
+
+  loadData(){
     let categoria_id = this.navParams.get("cat");
+    let loader = this.presentLoading();
     this.produtoService.findByCategoria(categoria_id)
     .subscribe(response =>{
       this.items = response['content'];
+      loader.dismiss();
       this.loadImageUrls();
     },
-  error=>{});
-
-  
+  error=>{
+    loader.dismiss();
+  });
   }
 
   loadImageUrls(){
@@ -53,5 +60,21 @@ export class ProdutosPage {
     console.log(produto_id);
     this.navCtrl.push('ProdutoDetailPage' , {prod :produto_id});
   }
+
+  presentLoading() {
+    let loader = this.loadingController.create({
+      content: "Aguarde..."
+    });
+    loader.present();
+    return loader;
+  }
+
+  doRefresh(refresher) {
+    this.loadData();
+    setTimeout(() => {
+        refresher.complete();
+    }, 1000);
+  }
+
 
 }
